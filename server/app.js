7e413e5f52f043c129a76ab4f5cb602ab047c54e
gracewinata <gracewinata1998@gmail.com>
2024-12-05 09:57:43 +0700

    merge seno & grace

--- server/app.js
+++ server/app.js
@@ -14,6 +14,8 @@ const port = 3000;
 
 /** Buat nyimpan semua roomstate dan skor setiap pemain didalamny */
 const roomStates = {};
+const allUsers = {};
+const allRooms = [];
 
 /** Inisialisasi Socket ( frontend mana saja yang bisa buat request ke socket kita) */
 const io = new Server(server, {
@@ -29,6 +31,80 @@ app.use(router);
 
 /** Connnection is made n log is ran everytime somebody clicks on 2 Player ( which leads to selectroompage being rendered )  */
 io.on("connection", (socket) => {
+  // tic tac toe seno
+  allUsers[socket.id] = {
+    socket: socket,
+    online: true,
+  };
+
+  socket.on("request_to_play", (data) => {
+    const currentUser = allUsers[socket.id];
+    currentUser.playerName = data.playerName;
+
+    let opponentPlayer;
+
+    for (const key in allUsers) {
+      const user = allUsers[key];
+      if (user.online && !user.playing && socket.id !== key) {
+        opponentPlayer = user;
+        break;
+      }
+    }
+
+    if (opponentPlayer) {
+      allRooms.push({
+        player1: opponentPlayer,
+        player2: currentUser,
+      });
+
+      currentUser.socket.emit("OpponentFound", {
+        opponentName: opponentPlayer.playerName,
+        playingAs: "circle",
+      });
+
+      opponentPlayer.socket.emit("OpponentFound", {
+        opponentName: currentUser.playerName,
+        playingAs: "cross",
+      });
+
+      currentUser.socket.on("playerMoveFromClient", (data) => {
+        opponentPlayer.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
+
+      opponentPlayer.socket.on("playerMoveFromClient", (data) => {
+        currentUser.socket.emit("playerMoveFromServer", {
+          ...data,
+        });
+      });
+    } else {
+      currentUser.socket.emit("OpponentNotFound");
+    }
+  });
+
+  socket.on("disconnect", function () {
+    const currentUser = allUsers[socket.id];
+    currentUser.online = false;
+    currentUser.playing = false;
+
+    for (let index = 0; index < allRooms.length; index++) {
+      const { player1, player2 } = allRooms[index];
+
+      if (player1.socket.id === socket.id) {
+        player2.socket.emit("opponentLeftMatch");
+        break;
+      }
+
+      if (player2.socket.id === socket.id) {
+        player1.socket.emit("opponentLeftMatch");
+        break;
+      }
+    }
+  });
+
+
+
   // chat
   console.log('a user connected', socket.id)
   socket.emit("welcome", "Hello" + socket.id)
